d8030b4f28e867398eb1b8c97e74eadf88a13dee
Khushi Agarwal <agrwlkhushi27@gmail.com>
2024-10-29 00:19:39 +0530

    home page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import {CssBaseline} from "@mui/material"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-  <CssBaseline/>
-  <div onContextMenu={(e)=> e.preventDefault()}>
-  <App />
-  </div>
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
