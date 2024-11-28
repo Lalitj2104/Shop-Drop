c0103cebdaa4d1482b5131bf42569c05c7610eba
Khushi Agarwal <agrwlkhushi27@gmail.com>
2024-11-28 21:41:43 +0530

    updates

--- frontend/src/routes/ProtectedRoute.jsx
+++ frontend/src/routes/ProtectedRoute.jsx
@@ -1,22 +1,21 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { Navigate } from 'react-router-dom'
-import toastOptions from '../constants/toast'
-import { toast } from 'react-toastify'
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+import toastOptions from "../constants/toast";
+import { toast } from "react-toastify";
 
-const ProtectedRoute = ({children}) => {
- const {isAuthenticated,authError} =useSelector(state=>state.userAuth)
+const ProtectedRoute = ({ children }) => {
+	const { isAuthenticated, authError } = useSelector((state) => state.userAuth);
 
-    const dispatch=useDispatch();
-    useEffect(()=>{
-        if(authError){
-            toast.error(authError,toastOptions);
-            dispatch({type:"CLEAR_AUTH_ERROR"})
-        }
-    },[authError])
+	const dispatch = useDispatch();
+	useEffect(() => {
+		if (authError) {
+			toast.error(authError, toastOptions);
+			dispatch({ type: "CLEAR_AUTH_ERROR" });
+		}
+	}, [authError]);
 
- return isAuthenticated?children:<Navigate to ="/login"/>
- 
-}
+	return isAuthenticated ? children : <Navigate to="/login" />;
+};
 
-export default ProtectedRoute
+export default ProtectedRoute;
