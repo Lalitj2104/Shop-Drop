9c50a6d14ac65a94392aac49b83f30c92977f597
Khushi Agarwal <agrwlkhushi27@gmail.com>
2024-12-11 02:21:46 +0530

    wishlist

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -3,163 +3,151 @@ import Product from "../models/product.js";
 import { message } from "../utils/message.js";
 import { Response } from "../utils/response.js";
 
-export const addToCart =async(req,res)=>{
-    try {
-        // console.log("working")
-        const {productId}=req.params;
-        const {quantity}=req.body;
-        if(quantity<=0){
-            return Response(res,400,false,message.invalidQuantityMessage);
-        }
-        
-        
-        const product=await Product.findById(productId);
-        if(!product){
-            return Response(res,400,false,message.noProductMessage);
-        }
-        //quantity available or not
-        if(product.available_quantity_delivery<quantity){
-            return Response(res, 400,false, message.insufficientQuantityMessage);
-        }
-        let cart =await Cart.findOne({userId:req.user._id});
-        console.log(cart);
-        if(!cart){
-            cart=await Cart.create({
-                userId:req.user._id,
-                products:[{
-                   productId: product._id,
-                    quantity,
-                    price:product.price,
-                    retailerId:product.retailerId
-            }]
-            })
-        }else{
-            const productExist=cart.products.findIndex((item)=>(item.productId.toString()===productId.toString()));
-            if(productExist>-1){
-                cart.products[productExist].quantity +=quantity;
-            }
-            else{
-                cart.products.push({
-                    productId:product._id,
-                    quantity,
-                    price:product.price,
-                    retailerId:product.retailerId
-                })
-            }
-        }
-        await cart.save();
-        Response(res,200,true,message.productAddedMessage,cart);
+export const addToCart = async (req, res) => {
+	try {
+		// console.log("working")
+		const { productId } = req.params;
+		const { quantity } = req.body;
+		if (quantity <= 0) {
+			return Response(res, 400, false, message.invalidQuantityMessage);
+		}
 
-    } catch (error) {
-        Response(res,500,false,error.message);
-    }
-}
+		const product = await Product.findById(productId);
+		if (!product) {
+			return Response(res, 400, false, message.noProductMessage);
+		}
+		//quantity available or not
+		if (product.available_quantity_delivery < quantity) {
+			return Response(res, 400, false, message.insufficientQuantityMessage);
+		}
+		let cart = await Cart.findOne({ userId: req.user._id });
+		console.log(cart);
+		if (!cart) {
+			cart = await Cart.create({
+				userId: req.user._id,
+				products: [
+					{
+						productId: product._id,
+						quantity,
+						price: product.price,
+						retailerId: product.retailerId,
+					},
+				],
+			});
+		} else {
+			const productExist = cart.products.findIndex(
+				(item) => item.productId.toString() === productId.toString()
+			);
+			if (productExist > -1) {
+				cart.products[productExist].quantity += quantity;
+			} else {
+				cart.products.push({
+					productId: product._id,
+					quantity,
+					price: product.price,
+					retailerId: product.retailerId,
+				});
+			}
+		}
+		await cart.save();
+		Response(res, 200, true, message.productAddedMessage, cart);
+	} catch (error) {
+		Response(res, 500, false, error.message);
+	}
+};
 
-export const getCart=async(req,res)=>{
-    try {
-        // console.log("working")
-        const cart=await Cart.findOne({userId:req.user._id})
-        .populate({
-            path: 'products.productId',
-            select: 'name description image price', 
-        });
-            // console.log(cart);
-        Response(res,200,true,message.cartFetchedMessage,cart);
-    } catch (error) {
-        Response(res,500,false,error.message);
-    }
-}
+export const getCart = async (req, res) => {
+	try {
+		// console.log("working")
+		const cart = await Cart.findOne({ userId: req.user._id }).populate({
+			path: "products.productId",
+			select: "name description image price",
+		});
+		// console.log(cart);
+		Response(res, 200, true, message.cartFetchedMessage, cart);
+	} catch (error) {
+		Response(res, 500, false, error.message);
+	}
+};
 
+export const removeProductFromCart = async (req, res) => {
+	try {
+		const { id } = req.params;
+		const product = await Product.findById(id);
+		console.log(product);
+		if (!product) {
+			return Response(res, 400, false, message.noProductMessage);
+		}
 
-export const removeProductFromCart=async(req,res)=>{
-    try {
+		const cart = await Cart.findOne({ userId: req.user._id }).populate({
+			path: "products.productId",
+			select: "name description image price",
+		});
+		console.log(cart);
+		if (cart) {
+			cart.products = cart.products.filter(
+				(item) => !(item?.productId?._id.toString() === id.toString())
+			);
+		}
+		await cart.save();
+		console.log(cart);
+		Response(res, 200, true, message.productRemovedMessage, cart);
+	} catch (error) {
+		Response(res, 500, false, error.message);
+	}
+};
 
-        const {id} =req.params;
-        const product=await Product.findById(id);
-        console.log(product);
-        if(!product){
-            return Response(res,400,false,message.noProductMessage);
+export const clearCart = async (req, res) => {
+	try {
+		const cart = await Cart.findOne({ userId: req.user._id });
 
-        }
-        
-        const cart=await Cart.findOne({userId:req.user._id})
-        .populate({
-            path: 'products.productId',
-            select: 'name description image price', 
-        });
-        console.log(cart);
-        if(cart){
-            cart.products=cart.products.filter((item)=>!(item?.productId?._id.toString()===id.toString()))
-        }
-        await cart.save();
-        console.log(cart);
-        Response(res,200,true,message.productRemovedMessage,cart)
-    } catch (error) {
-        Response(res,500,false,error.message);
-    }
-}
+		if (!cart) {
+			return Response(res, 400, false, message.cartEmptyMessage);
+		}
+		cart.products = [];
+		await cart.save();
+		Response(res, 200, true, message.cartClearMessage, cart);
+	} catch (error) {
+		Response(res, 500, false, error.message);
+	}
+};
 
+export const updateCart = async (req, res) => {
+	try {
+		const { id, quantity } = req.body;
+		if (!id || !quantity) {
+			return Response(res, 400, false, message.idNotFoundMessage);
+		}
 
-export const clearCart=async(req,res)=>{
-    try {
+		let cart = await Cart.findOne({ userId: req.user._id });
 
-        const cart=await Cart.findOne({userId: req.user._id});
+		if (!cart) {
+			return Response(res, 404, false, "Cart not found.");
+		}
 
-        if(!cart){
-            return Response(res,400,false,message.cartEmptyMessage);
-        }
-        cart.products=[];
-        await cart.save();
-        Response(res,200,true,message.cartClearMessage,cart);
-        
-    } catch (error) {
-        Response(res,500,false,error.message);
-    }
-}
+		// Find the product in the cart
+		const productIndex = cart.products.findIndex(
+			(item) => item.productId.toString() === id.toString()
+		);
 
+		if (productIndex === -1) {
+			return Response(res, 404, false, "Product not found in the cart.");
+		}
+		if (quantity < 1) {
+			return Response(res, 400, false, "Min quantity");
+		} else {
+			cart.products[productIndex].quantity = quantity;
+		}
 
-export const updateCart=async(req,res)=>{
-    try {
-        const {id,quantity}=req.body;
-        if(!id ||!quantity){
-            return Response(res,400,false,message.idNotFoundMessage);
-        }
+		// Save the updated cart
+		await cart.save();
+		cart = await Cart.findOne({ userId: req.user._id }).populate({
+			path: "products.productId",
+			select: "name description image price",
+		});
 
-        let cart=await Cart.findOne({userId:req.user._id})
-        
-        
-        if (!cart) {
-            return Response(res, 404, false, "Cart not found.");
-        }
-
-        // Find the product in the cart
-        const productIndex = cart.products.findIndex(
-            (item) => item.productId.toString() === id.toString()
-        );
-
-        if (productIndex === -1) {
-            return Response(res, 404, false, "Product not found in the cart.");
-        }
-        if (quantity < 1) {
-            return Response(res,400,false,"Min quantity")
-        } else {
-            
-            cart.products[productIndex].quantity = quantity;
-        }
-
-        // Save the updated cart
-        await cart.save();
-        cart=await Cart.findOne({userId:req.user._id})
-        .populate({
-            path: 'products.productId',
-            select: 'name description image price', 
-        });
-
-        Response(res, 200, true, "Cart updated successfully.", cart);
-
-        
-    } catch (error) {
-        Response(res,500,false,error.message);
-    }
-}
-
+		Response(res, 200, true, "Cart updated successfully.", cart);
+	} catch (error) {
+		Response(res, 500, false, error.message);
+	}
+};
--- frontend/src/pages/Wishlist/Wishlist.jsx
+++ frontend/src/pages/Wishlist/Wishlist.jsx
@@ -1,41 +1,23 @@
-import "../../styles/Wishlist.css"; // Updated styles for horizontal cards
+import "../../styles/Wishlist.css";
 import Header from "../../components/Header/Header";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
-import { getWishList, removeWishList } from "../../redux/Actions/wishListAction";
+import {
+	getWishList,
+	removeWishList,
+} from "../../redux/Actions/wishListAction";
 import { addToCart } from "../../redux/Actions/cartAction";
 
 function Wishlist() {
-  const dispatch = useDispatch();
+	const dispatch = useDispatch();
 	const { wishList } = useSelector((state) => state.wishListAuth);
-	// const WishlistProducts = [
-	// 	{
-	// 		id: 1,
-	// 		name: "Flared Sleeves Sweater",
-	// 		price: 19.99,
-	// 		image: "/Flared Sleeves Sweater.jpg",
-	// 		color: "Beige",
-	// 	},
-	// 	{
-	// 		id: 2,
-	// 		name: "V-Neck Sweater",
-	// 		price: 35.99,
-	// 		image: "/V-Neck Sweater.jpg",
-	// 		color: "Light Blue",
-	// 	},
-	// 	{
-	// 		id: 3,
-	// 		name: "Casual Knitwear",
-	// 		price: 25.99,
-	// 		image: "/Casual Knitwear.jpg",
-	// 		color: "Gray",
-	// 	},
-  // ];
- 
-  useEffect(() => {
-    dispatch(getWishList());
-    console.log("Wishlist: ", wishList);
-  }, [dispatch]);
+
+	useEffect(() => {
+		dispatch(getWishList());
+	}, [dispatch]);
+
+	const hasItems =
+		Array.isArray(wishList?.products) && wishList.products.length > 0;
 
 	return (
 		<>
@@ -44,47 +26,60 @@ function Wishlist() {
 				<h2 className="wishlist-title">Your Wishlist</h2>
 				<p className="wishlist-subtitle">Keep track of items you love</p>
 
-				<div className="wishlist-list">
+				{hasItems ? (
 					<div className="wishlist-list">
-						{Array.isArray(wishList?.products) ? (
-							wishList.products.map((product) => (
-								<div
-									className="wishlist-horizontal-card"
-									key={product._id || product.id}
-								>
-									{/* Product Image */}
-									<div className="wishlist-image-wrapper">
-										<img
-											src={product?.image?.url}
-											alt={product?.name}
-											className="wishlist-image"
-										/>
-									</div>
-
-									{/* Product Details */}
-									<div className="wishlist-details">
-                    <h3 className="wishlist-name">{product?.name}</h3>
-                    <p>{product?.description}</p>
-										<p className="wishlist-price">$ {product?.price}</p>
-									</div>
-
-									{/* Action Buttons */}
-									<div className="wishlist-actions">
-										<button className="wishlist-add-to-cart" onClick={() => dispatch(addToCart(product?._id, 1))}>Add to Bag</button>
-                    <button className="wishlist-remove" onClick={() => dispatch(removeWishList(product?._id))}>🗑</button>
-									</div>
+						{wishList.products.map((product) => (
+							<div
+								className="wishlist-horizontal-card"
+								key={product._id || product.id}
+							>
+								{/* Product Image */}
+								<div className="wishlist-image-wrapper">
+									<img
+										src={product?.image?.url}
+										alt={product?.name}
+										className="wishlist-image"
+									/>
 								</div>
-							))
-						) : (
-							<p>Your wishlist is empty.</p>
-						)}
-					</div>
-				</div>
 
-				{/* Add All Button */}
-				<div className="wishlist-add-all">
-					<button>Add All to Bag</button>
-				</div>
+								{/* Product Details */}
+								<div className="wishlist-details">
+									<h3 className="wishlist-name">{product?.name}</h3>
+									<p>{product?.description}</p>
+									<p className="wishlist-price">$ {product?.price}</p>
+								</div>
+
+								{/* Action Buttons */}
+								<div className="wishlist-actions">
+									<button
+										className="wishlist-add-to-cart"
+										onClick={() => dispatch(addToCart(product?._id, 1))}
+									>
+										Add to Bag
+									</button>
+									<button
+										className="wishlist-remove"
+										onClick={() => dispatch(removeWishList(product?._id))}
+									>
+										🗑
+									</button>
+								</div>
+							</div>
+						))}
+					</div>
+				) : (
+					<div className="wishlist-empty">
+						<p className="wishlist-empty-message">
+							No items in your wishlist. <a href="/shop">Continue Shopping</a>
+						</p>
+					</div>
+				)}
+
+				{hasItems && (
+					<div className="wishlist-add-all">
+						<button>Add All to Bag</button>
+					</div>
+				)}
 			</div>
 		</>
 	);
