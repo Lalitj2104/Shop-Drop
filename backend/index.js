06bddd03c0505d66d1a94b1d091b7ebd1878b1cb
Lalit <lalitjindal0295@gmail.com>
2025-03-13 16:54:11 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -27,8 +27,10 @@ app.use(
 		origin: "*",
 		methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
 		credentials: true,
+        allowedHeaders: 'Content-Type,Authorization'
 	})
 );
+app.options('*', cors());
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: false }));
