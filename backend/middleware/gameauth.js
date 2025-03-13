7d1e6c9fc9937bf46292869db8484d8ce7c6cf7a
Lalit <lalitjindal0295@gmail.com>
2025-03-13 16:25:51 +0530

    commit

--- backend/middleware/gameauth.js
+++ backend/middleware/gameauth.js
@@ -1,33 +1,29 @@
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 import Game from "../models/gameUser.js";
 
-export const gameAuth= async (req, res, next) => {
-    try {
-        const token = req.headers.authorization.split(":")[1];
+export const gameAuth = async (req, res, next) => {
+	try {
+		const token = req.headers.authorization.split(":")[1];
 
-        if (!token)
-            return res
-                .status(403)
-                .json({
-                    Error: "Authorization Revoked . Please provide valid auth-headers",
-                });
+		if (!token)
+			return res.status(403).json({
+				Error: "Authorization Revoked . Please provide valid auth-headers",
+			});
 
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        if (!decoded) return res.status(403).json({ Error: "Token Error" });
+		const decoded = jwt.verify(token, process.env.JWT_SECRET);
+		if (!decoded) return res.status(403).json({ Error: "Token Error" });
 
-        const user = await Game.findOne(
-            { email: decoded.email },
-            { _id: 0, password: 0, tokens: 0 }
-        );
-        // console.log(doctor)
-        req.email = user.email;
-        req.user = user;
-        next();
-    } catch (error) {
-        return res
-            .status(403)
-            .json({
-                Error: "Authorization Revoked . Please provide valid auth-headers",
-            });
-    }
+		const user = await Game.findOne(
+			{ email: decoded.email },
+			{ _id: 0, password: 0, tokens: 0 },
+		);
+		// console.log(doctor)
+		req.email = user.email;
+		req.user = user;
+		next();
+	} catch (error) {
+		return res.status(403).json({
+			Error: "Authorization Revoked . Please provide valid auth-headers",
+		});
+	}
 };
