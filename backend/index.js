58b479b23b08174d7181bd3cc680f2e2d7f68e30
Lalit <lalitjindal0295@gmail.com>
2025-03-13 16:34:42 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,46 @@
-import app from "./app.js"
+
 import connectDB from "./config/db.js"
 import cloudinary from "cloudinary"
 import {io} from "socket.io-client"
 import initializeGame  from "./utils/game-logic.js"
+import express, { Router } from "express";
+import dotenv from "dotenv";
+import userRouter from "./routes/userRoute.js";
+import cookieParser from "cookie-parser";
+import retailerRouter from "./routes/retailerRoute.js";
+import productRouter from "./routes/productRoute.js";
+import wishListRouter from "./routes/wishList.js";
+import cors from "cors";
+import orderRouter from "./routes/orderRoute.js";
+import cartRouter from "./routes/cartRoute.js";
+import { reviewRouter } from "./routes/reviewRoute.js";
+import { router } from "./routes/gameRoute.js";
+import { adminRouter } from "./routes/adminRoute.js";
 
+dotenv.config({ path: "./config/config.env" });
 connectDB();
+
+const app = express();
+
+app.use(
+	cors({
+		origin: "*",
+		methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+		credentials: true,
+	})
+);
+
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+	res.send("your server is active");
+});
+
+
+
+
 cloudinary.config({
     cloud_name:process.env.CLOUD_NAME,
     api_key:process.env.CLOUD_API_KEY,
@@ -28,3 +64,12 @@ Socketio.on("connection",client=>{
     initializeGame(Socketio,client);
 })
 
+app.use(router);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/retailer", retailerRouter);
+app.use("/api/v1/product", productRouter);
+app.use("/api/v1/wishList", wishListRouter);
+app.use("/api/v1/order",orderRouter);
+app.use("/api/v1/cart",cartRouter);
+app.use("/api/v1/review",reviewRouter)
+app.use("/api/v1/admin",adminRouter);
